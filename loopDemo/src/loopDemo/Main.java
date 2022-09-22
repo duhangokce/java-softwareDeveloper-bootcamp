56c90d0a1a7bac525a01566e50506661a1085b97
dgokce <dgokc@pc>
2022-09-22 11:57:01 +0300

    loopDemo

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,30 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//For
+		for(int i = 1; i < 10; i++);{
+			System.out.println();
+		}
+		System.out.println("For loop ended");
+		//While
+		int i = 1;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While loop finished");
+		//Do-while Loop
+		int j = 0;
+		do {
+			System.out.println(j);
+			j+=4;
+		}while(j<=16);
+		
+				
+
+	}
+
+}
