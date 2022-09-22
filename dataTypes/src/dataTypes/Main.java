73699d43e99d58cb0de332fc6e6dc4c97427a8a4
dgokce <dgokce@pc>
2022-09-22 11:55:24 +0300

    dataTypes

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,23 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Primitive Types
+		
+		int integer = 5;  //-+2billion
+		
+		String name = "Duhan";
+		
+		double decimal = 3.1;
+		
+		char character = 'a'; //not used much
+		
+		boolean isTrueOrFalse = true;
+		
+		
+
+	}
+
+}
