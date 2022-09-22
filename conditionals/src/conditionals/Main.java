938d127486995c9b40fbd499a8a02180a04c6b2c
dgokce <dgokce@pc>
2022-09-22 11:56:10 +0300

    conditionals

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int number = 20;
+		if (number < 20) {
+			
+			System.out.println(number + " is "  + "less than 20");
+		} else if(number>20) {
+			System.out.println(number + " is "  +"greater than 20");
+		}else {
+			System.out.println(number + " is "  +"equal to 20");
+		}
+
+	}
+
+}
